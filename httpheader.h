d3e6378aeb0504500c60a64193902bac4bf0194d
ljianhui <ljianhui2012@163.com>
2014-02-26 00:41:30 +0800

    定义HTTP消息结构类的部分接口

--- httpheader.h
+++ httpheader.h
@@ -1,6 +1,25 @@
 #ifndef HTTPHEADER_H_INCLUDED
 #define HTTPHEADER_H_INCLUDED
 
+#include "object.h"
+#include <string>
 
+using std::string;
+
+class HttpHeader : public Object
+{
+    public:
+        HttpHeader(Object *parent = NULL);
+        HttpHeader(const HttpHeader &header);
+        HttpHeader(const string &head);
+        HttpHeader(const char *head);
+        HttpHeader& operator=(const HttpHeader &header);
+        virtual ~HttpHeader();
+
+        enum Method
+        {
+            GET, POST, HEAD, PUT, DELETE
+        };
+};
 
 #endif // HTTPHEAD_H_INCLUDED
